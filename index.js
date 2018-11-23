54ebf8310315f31b57551b60f235b4a8732e4557
Egor Litvinchuk <elitvinchuk@gojuno.com>
2018-11-23 12:08:14 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended"
+  ],
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "rules": {
+    "prefer-const": "warn",
+    "react/prop-types": ["enabled", {
+      "ignore": ["dispatch"]
+    }],
+    "no-unused-vars": ["error", { "ignoreRestSiblings": true }]
+  },
+  "globals": {
+    "__REVISION__" : false
+  }
+}
